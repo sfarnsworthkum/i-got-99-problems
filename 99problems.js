1562fa332fe7fcf9dba3a119a123747fe30fd3d3
Sarah Farnsworth-kumli <sarah@stormpath.com>
2017-11-08 12:02:54 -0800

    rotatearray

--- 99problems.js
+++ 99problems.js
@@ -12,3 +12,24 @@ function solve(s) {
   }
   return max;
 }
+
+//rotateArray
+//Write a function called rotateArray which accepts an array and a number (let's call it n) and rotates the array n times. The function should return the array.
+
+// rotateArray([1,2,3,4,5,6,7], 3) // [5,6,7,1,2,3,4]
+// rotateArray([4,1,2,10,6,3], 1) // [3,4,1,2,10,6]
+// rotateArray([4,1,2,10,6,3], 100) // [2,10,6,3,4,1]
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+function rotateArray(arr, n) {
+  // if(n > arr.length) {
+  //   num = n % arr.length;
+  // } else {
+  // num = n;
+  // }
+  let num = n > arr.length ? n % arr.length : n;
+  arr.unshift(...arr.splice(arr.length - num));
+  return arr;
+}
