4843a3b9c85233d708550ee8b575ae9a1095c062
Sarah Farnsworth-kumli <sarah@stormpath.com>
2017-11-08 17:11:17 -0800

    a few more

--- 99problems.js
+++ 99problems.js
@@ -188,3 +188,54 @@ function findUniq(arr) {
     }
   }
 }
+
+//Detect Pangram
+
+//A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+//Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+function isPangram(string) {
+  string = string.toLowerCase();
+  var alpha = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+  var pt = 0;
+  while (pt < string.length) {
+    if (alpha.indexOf(string[pt]) >= 0) {
+      alpha.splice(alpha.indexOf(string[pt]), 1);
+    }
+    pt++;
+  }
+  if (alpha.length > 0) {
+    console.log(alpha);
+    return false;
+  }
+  console.log(alpha);
+  return true;
+}
