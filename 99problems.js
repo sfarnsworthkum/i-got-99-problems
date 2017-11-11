7a78d4eebf319415c11d2a93177d1d563e02b82f
Sarah Farnsworth-kumli <sarah@stormpath.com>
2017-11-11 12:54:15 -0800

    reverse

--- 99problems.js
+++ 99problems.js
@@ -531,6 +531,40 @@ isPalindrome('tacocat') // true
 isPalindrome('amanaplanacanalpanama') // true
 isPalindrome('amanaplanacanalpandemonium') // false
 
+//someRecursive
+//Write a recursive function called someRecursive which accepts an array and a callback. The function returns true if a single value in the array returns true when passed to the callback. Otherwise it returns false.
+
+const isOdd = val => val % 2 !== 0;
+
+someRecursive([1,2,3,4], isOdd) // true
+someRecursive([4,6,8,9], isOdd) // true
+someRecursive([4,6,8], isOdd) // false
+someRecursive([4,6,8], val => val > 10); // false
+function isPalindrome(string){
+  if(string.length <= 1){
+    return true;
+  }
+  if(string[0] === string[string.length -1]){
+    return (isPalindrome(string.slice(1, string.length - 1)))
+  } else{
+    return false;
+  }
+}
+
+//reverse 
+//Write a  recursive function called reverse which accepts a string and returns a new string in reverse.
+function reverse(str){
+  if(str.length === 1){
+    return str[0];
+  } else {
+    return reverse(str.slice(1)) + str[0];
+  }
+  
+}
+
+reverse('awesome') // 'emosewa'
+reverse('rithmschool') // 'loohcsmhtir'
+
 
 
     
