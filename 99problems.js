d61b00d967261f6eb95cf043e6d2cf3dbb7bee7f
Sarah Farnsworth-kumli <sarah@stormpath.com>
2017-11-10 16:57:35 -0800

    constructnote

--- 99problems.js
+++ 99problems.js
@@ -388,6 +388,42 @@ function longestFall(nums) {
   return maxCount;
 }
 
+//constructNote
+
+// You are trying to construct a message but you only have a limited collection of letters.  Determine if the message can be built with the letters that you are given.
+
+// Assume that there are only lowercase letters and no space or special characters in both the message and the letters.
+
+// Input: message {string}, letters {string}
+
+// Output: boolean
+
+// Constraints: 
+// if M is the length of messages and N is the length of letters:
+
+// Time Complexity: O(M+N) 
+// Space Complexity: O(N) 
+
+function constructNote(message, letters) {
+    var lettersArr = letters.split('');
+    var messageArr = message.split('');
+    var countObj = {};
+    for (let i = 0; i < lettersArr.length; i++) {
+        countObj[lettersArr[i]] = (countObj[lettersArr[i]] || 0) + 1;
+    }
+    for (let i = 0; i < messageArr.length; i++) {
+        if(countObj[messageArr[i]]) {
+            countObj[messageArr[i]]--;
+            if(countObj[messageArr[i]] < 0) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
     
 
 
