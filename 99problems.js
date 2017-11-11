647b0ee888d0533ec36277f1ea1deeb5b9c69301
Sarah Farnsworth-kumli <sarah@stormpath.com>
2017-11-11 12:44:49 -0800

    isPalindrome

--- 99problems.js
+++ 99problems.js
@@ -513,6 +513,25 @@ fib(10) // 55
 fib(28) // 317811
 fib(35) // 9227465
 
+//isPalindrome
+//Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward). Otherwise it returns false.
+function isPalindrome(string){
+  if(string.length <= 1){
+    return true;
+  }
+  if(string[0] === string[string.length -1]){
+    return (isPalindrome(string.slice(1, string.length - 1)))
+  } else{
+    return false;
+  }
+}
+isPalindrome('awesome') // false
+isPalindrome('foobar') // false
+isPalindrome('tacocat') // true
+isPalindrome('amanaplanacanalpanama') // true
+isPalindrome('amanaplanacanalpandemonium') // false
+
+
 
     
 
