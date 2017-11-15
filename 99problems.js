33ebe9d774a1b61b3255264c2d8c03818e3baaf3
Sarah Farnsworth-kumli <sarah@stormpath.com>
2017-11-15 11:36:15 -0700

    insert recursive

--- 99problems.js
+++ 99problems.js
@@ -764,6 +764,30 @@ inarySearchTree.prototype.insertIteratively = function (value) {
   }
 };
 
+BinarySearchTree.prototype.insertRecursively = function (value, node) {
+  var newNode = new Node(value);
+  if(!this.root) {
+    this.root = newNode;
+    return this;
+  }
+  if(node === undefined) {
+    node = this.root;
+  }
+  if(newNode.value < node.value) {
+    if(!node.left){
+      node.left = newNode;
+      return this;
+    }return this.insertRecursively(value, node.left);
+  } 
+  if(newNode.value > node.value) {
+    if(!node.right) {
+      node.right = newNode;
+      return this;
+    }node = node.right;
+    return this.insertRecursively(value, node.right);
+  }
+  
+};
 
 
 
