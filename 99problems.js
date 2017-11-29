c844ac4071648d522a44df620e25bafe018f8b86
Sarah Farnsworth-kumli <sarah@stormpath.com>
2017-11-28 19:48:04 -0800

    stairs redo

--- 99problems.js
+++ 99problems.js
@@ -962,6 +962,11 @@ HashTable.prototype.get = function(key) {
   }
 }
 
+//stairs 
+
+//Write a function called stairs which accepts n number of stairs. Imagine that a person is standing at the bottom of the stairs and wants to reach the top and the person can climb either 1 stair or 2 stairs at a time. Your function should return the number of ways the person can reach the top by only climbing 1 or 2 stairs at a time.
+
+
 
 
 
